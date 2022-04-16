331814e863655ca4d55205eab55132e00ff83032
Matheus Antunes <98540256+antunes-lima@users.noreply.github.com>
2022-04-16 19:23:39 -0300

    Add files via upload

---
+++ desafio 4 - SQLITE.sql
@@ -0,0 +1,44 @@
+/* QUESTAO 1
+Crie uma view (SELLER_STATS) para mostrar por fornecedor, 
+a quantidade de itens enviados, 
+o tempo médio de postagem após a aprovação da compra, 
+a quantidade total de pedidos de cada Fornecedor, 
+note que trabalharemos na mesma query com 2 granularidades diferentes.
+*/
+CREATE VIEW IF NOT EXISTS SELLER_STATS AS
+	SELECT 
+		Vendedores.seller_id AS Vendedor,
+		count(Items.product_id) AS Quantidade_items,
+		avg(julianday(Pedidos.order_delivered_carrier_date) - julianday(Pedidos.order_approved_at)) AS Tempo_medio_postagem,
+		count(DISTINCT Pedidos.order_id) AS Quantidade_pedidos
+	FROM olist_orders_dataset AS Pedidos
+	INNER JOIN olist_order_items_dataset AS Items ON Items.order_id = Pedidos.order_id
+	INNER JOIN olist_sellers_dataset AS Vendedores ON Vendedores.seller_id = Items.seller_id
+	WHERE Pedidos.order_status NOT IN ('canceled')
+	GROUP BY Vendedor
+
+/* QUESTAO 2
+Queremos dar um cupom de 10% do valor da última compra do cliente. 
+Porém os clientes elegíveis a este cupom devem ter feito uma compra anterior a última (a partir da data de aprovação do pedido) 
+que tenha sido maior ou igual o valor da última compra. 
+Crie uma querie que retorne os valores dos cupons para cada um dos clientes elegíveis.
+*/
+SELECT * FROM
+	(SELECT *,
+		lag(Valor_pedido) OVER (PARTITION BY Cliente_ID ORDER BY Data_pedido) AS Valor_pedido_anterior,
+		Valor_pedido*0.1 AS Valor_cupom
+	FROM (
+		SELECT 
+			Clientes.customer_unique_id AS Cliente_ID,
+			Pedidos.order_id AS Pedido_ID,
+			Pedidos.order_approved_at AS Data_pedido,
+			sum(Pagamentos.payment_value) AS Valor_pedido
+		FROM olist_customers_dataset AS Clientes
+		INNER JOIN olist_orders_dataset AS Pedidos ON Pedidos.customer_id = Clientes.customer_id
+		INNER JOIN olist_order_payments_dataset AS Pagamentos ON Pagamentos.order_id = Pedidos.order_id
+		WHERE Pedidos.order_status NOT IN ('canceled')
+		GROUP BY Cliente_ID, Pedido_ID, Data_pedido
+		ORDER BY Cliente_ID
+		)
+	)
+WHERE Valor_pedido_anterior >= Valor_pedido
---
+++ desafio 5 - SQLITE.sql
@@ -0,0 +1,72 @@
+/* QUESTAO 1
+Crie os índices apropriadaos para as tabelas do nosso modelo de dados com o intuito de melhorar a performance.
+*/
+CREATE INDEX idx_customer ON olist_customers_dataset (customer_id);
+CREATE INDEX idx_customer2 ON olist_orders_dataset (customer_id);
+
+CREATE INDEX idx_order ON olist_orders_dataset (order_id);
+CREATE INDEX idx_order2 ON olist_order_items_dataset (order_id);
+CREATE INDEX idx_order3 ON olist_order_payments_dataset (order_id);
+CREATE INDEX idx_order4 ON olist_order_reviews_dataset (order_id);
+
+CREATE INDEX idx_product ON olist_order_items_dataset (product_id);
+CREATE INDEX idx_product2 ON olist_products_dataset (product_id);
+
+CREATE INDEX idx_seller ON olist_order_items_dataset (seller_id);
+CREATE INDEX idx_seller2 ON olist_sellers_dataset (seller_id);
+
+CREATE INDEX idx_geolocation ON olist_geolocation_dataset (geolocation_zip_code_prefix);
+CREATE INDEX idx_geolocation2 ON olist_customers_dataset (customer_zip_code_prefix);
+CREATE INDEX idx_geolocation3 ON olist_sellers_dataset (seller_zip_code_prefix);
+
+/* QUESTAO 2 (Opcional)
+Crie índices cluesterizados. Lembre-se que, para isso, você deverá recriar a tabela para poder criar as Primary e Foreign Keys.
+*/
+CREATE TABLE "clustered_customers" (
+"customer_id" TEXT PRIMARY KEY,
+"customer_unique_id" TEXT,
+"customer_zip_code_prefix" INTEGER,
+"customer_city" TEXT,
+"customer_state" TEXT
+) WITHOUT ROWID;
+INSERT INTO clustered_customers SELECT * FROM olist_customers_dataset;
+CREATE TABLE "clustered_orders" (
+"order_id" TEXT PRIMARY KEY,
+"customer_id" TEXT,
+"order_status" TEXT,
+"order_purchase_timestamp" TEXT,
+"order_approved_at" TEXT,
+"order_delivered_carrier_date" TEXT,
+"order_delivered_customer_date" TEXT,
+"order_estimated_delivery_date" TEXT
+) WITHOUT ROWID;
+INSERT INTO clustered_orders SELECT * FROM olist_orders_dataset;
+CREATE TABLE "clustered_sellers" (
+"seller_id"	TEXT PRIMARY KEY,
+"seller_zip_code_prefix"	INTEGER,
+"seller_city"	TEXT,
+"seller_state"	TEXT
+) WITHOUT ROWID;
+INSERT INTO clustered_sellers SELECT * FROM olist_sellers_dataset;
+CREATE TABLE "clustered_products" (
+"product_id" TEXT PRIMARY KEY,
+"product_category_name" TEXT,
+"product_name_lenght" REAL,
+"product_description_lenght" REAL,
+"product_photos_qty" REAL,
+"product_weight_g" REAL,
+"product_length_cm" REAL,
+"product_height_cm" REAL,
+"product_width_cm" REAL
+) WITHOUT ROWID;
+INSERT INTO clustered_products SELECT * FROM olist_products_dataset;
+DROP TABLE olist_customers_dataset;
+DROP TABLE olist_orders_dataset;
+DROP TABLE olist_sellers_dataset;
+DROP TABLE olist_products_dataset;
+/* OBS: 
+Tabela order_payments usa a KEY estrangeira order_id, não tem chave primária
+Tabela order_items não tem valores únicos de order_item_id
+Tabela geolocation não tem valores únicos de zipcode_prefix
+Tabela order_reviews não tem valores únicos de review_id
+*/
