f391739cfa08ac266c680e9e60cf72ca1f23a5e2
Matheus Antunes <98540256+antunes-lima@users.noreply.github.com>
2022-04-16 19:19:04 -0300

    Update desafio 1 - SQLITE.sql

--- desafio 1 - SQLITE.sql
+++ desafio 1 - SQLITE.sql
@@ -5,6 +5,7 @@ SELECT * FROM olist_order_payments_dataset
 where payment_type = 'boleto'
 OR payment_type = 'voucher'
 
+
 /* QUESTAO 2
 Retorne os campos da tabela de produtos e calcule o volume de cada produto em um novo campo.
 */
@@ -12,11 +13,13 @@ SELECT 	*,
 		product_length_cm * product_width_cm * product_height_cm AS product_volume_cm_3
 FROM olist_products_dataset
 
+
 /* QUESTAO 3
 Retorne somente os reviews que não tem comentários.
 */
 SELECT * FROM olist_order_reviews_dataset
 WHERE review_comment_message IS NULL
+
 
 /* QUESTAO 4
 Retorne pedidos que foram feitos somente no ano de 2017.
@@ -24,6 +27,7 @@ Retorne pedidos que foram feitos somente no ano de 2017.
 SELECT * FROM olist_orders_dataset
 WHERE strftime('%Y', order_purchase_timestamp) = '2017'
 ORDER BY order_purchase_timestamp
+
 
 /* QUESTAO 5
 Encontre os clientes do estado de SP e que não morem na cidade de São Paulo.
